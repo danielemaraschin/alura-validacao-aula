b321d2c145158b98a827cb50a8cb7051c2648b59
danielemaraschin <danikmaraschin@gmail.com>
2020-08-29 15:26:58 +0200

    Working with split, substr, includes, replace

--- paginas/services/validarCPF.js
+++ paginas/services/validarCPF.js
@@ -1,3 +1,22 @@
+export const validarCPF = input =>  {
+    const cpfNumeros = input.value.replace(".", "");
+
+    if (ehUmCpfComNumerosRepetidos (cpfNumeros)) {
+        input.setCustomValidity("Este não é um CPF válido");
+        return;
+    }
+
+const primeiraParteCPF = cpfNumeros.substr(0,9).split("");
+const primeiroDigitoCPF = Number(cpfNumeros.chartAt(9)); 
+const primeiroDigitoCalculado = calcularDigito(primeiraParteCPF,10);
+
+
+    input.setCustomValidity("");
+    return;
+};
+
+
+
 const ehUmCpfComNumerosRepetidos = (cpf) => {
     const cfpsInvalidos =[
         "11111111111",
@@ -12,18 +31,24 @@ const ehUmCpfComNumerosRepetidos = (cpf) => {
     ];
     return cfpsInvalidos.includes(cpf);
 }
-
-
-export const validarCPF = input =>  {
-    const cpfNumeros = input.value.replace(".", "");
-
-    if (ehUmCpfComNumerosRepetidos (cpfNumeros)) {
-        input.setCustomValidity("Este não é um CPF válido");
-        return;
+const calcularTotal = (multiplicador) => (resultado, numeroAtual) => resultado +numeroAtual*multiplicador
+const calcularDigito = (Partecpf, multiplicador) =>{
+//valor total = multiplicacao dos numeros
+// resto= total % MODULO 11 (TOTAL/11);
+//digito = 11- resto 
+//(em caso de o digito ser maior que 9 retorna 0)
+//primeiro digito começa a multip em 10
+//segundo digito comeca a multiplicacao em 11
+    const total = Partecpf.reduce(calcularTotal(multiplicador),0);
+    const resto = total % 11;
+    
+    const digito = 11 - resto;
+    if (resto > 9){
+        digito = 0;
     }
+    	return digito;
+}
 
-    
-    
- input.setCustomValidity("");
- return;
-};
+
+
+
--- paginas/services/validarCPFduvidas.js
+++ paginas/services/validarCPFduvidas.js
@@ -1,21 +1,33 @@
 const cpfNumeros = input.value.replace(".", "");
-var cpfsInvalidos = ["11111111111",
-"22222222222",
-"33333333333",
-"44444444444",
-"55555555555",
-"66666666666",
-"77777777777",
-"88888888888",
-"99999999999"];
 
-function  ehUmCpfComNumerosRepetidos (){
-    
-    for (var posicao = 0; posicao < ehUmCpfComNumerosRepetidos.length; posicao++) {
-            
-        if (cpfNumeros == ehUmCpfComNumerosRepetidos[posicao] ) {
+cpfNumeros = cpfNumeros.substr(0,5);
+
+const ehUmCpfComNumerosRepetidos = (cpf) => {
+    var cpfsInvalidos = ["11111111111",
+    "22222222222",
+    "33333333333",
+    "44444444444",
+    "55555555555",
+    "66666666666",
+    "77777777777",
+    "88888888888",
+    "99999999999"];
+        
+    for (var posicao = 0; posicao < cpfsInvalidos.length; posicao++) {
+        if (cpf == cpfsInvalidos[posicao] ) {
             input.setCustomValidity("Este não é um CPF válido");
-            break;
+            return;
         }
-    }; 
-};
+
+    }
+}
+
+....................................
+var NomeString = "DANIELE";
+
+NomeString = NomeString.split("");
+console.log(NomeString)
+
+no console: 
+retorna array = [D, A,N , I, E, L, E];
+
