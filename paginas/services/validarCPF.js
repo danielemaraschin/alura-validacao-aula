5f275da4314a31014d374075447fa10467b60ca8
danielemaraschin <danikmaraschin@gmail.com>
2020-08-30 13:36:18 +0200

    working with CPF calculation and function reduce

--- paginas/services/validarCPF.js
+++ paginas/services/validarCPF.js
@@ -9,7 +9,25 @@ export const validarCPF = input =>  {
 const primeiraParteCPF = cpfNumeros.substr(0,9).split("");
 const primeiroDigitoCPF = Number(cpfNumeros.chartAt(9)); 
 const primeiroDigitoCalculado = calcularDigito(primeiraParteCPF,10);
+// para vermos se é um cpf válido, devemos verificar o valor do digito
+// multiplicando cada numero do cpf do 10 ao 2.
+//024.495.290-63
+//0x10,2x9,4x8,4x7... e vai somando esses resultados
 
+if (primeiroDigitoCPF !== primeiroDigitoCalculado){
+    input.setCustomValidity("Este não é um CPF válido");
+    return;
+}
+
+
+const segundaParteCPF = cpfNumeros.substr(0,10).split("");
+const segundoDigitoCPF = Number(cpfNumeros.chartAt(10)); 
+const segundoDigitoCalculado = calcularDigito(primeiraParteCPF,11);
+
+if (segundoDigitoCPF !== segundoDigitoCalculado){
+    input.setCustomValidity("Este não é um CPF válido");
+    return;
+}
 
     input.setCustomValidity("");
     return;
@@ -31,7 +49,10 @@ const ehUmCpfComNumerosRepetidos = (cpf) => {
     ];
     return cfpsInvalidos.includes(cpf);
 }
-const calcularTotal = (multiplicador) => (resultado, numeroAtual) => resultado +numeroAtual*multiplicador
+//multiplicador é o primeiro parametro pq eh o q recebe no calcular total e
+// dps os parametros sao o do reduce (o resultado e o item que ele esta no array - ou seja, o numero atual)
+//resultado + numero atual * multiplicador --
+const calcularTotal = (multiplicador) => (resultado, numeroAtual) => resultado +numeroAtual*multiplicador--
 const calcularDigito = (Partecpf, multiplicador) =>{
 //valor total = multiplicacao dos numeros
 // resto= total % MODULO 11 (TOTAL/11);
